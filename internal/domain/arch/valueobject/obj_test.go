674f66562a723f7f3bbfa925bd753fd2aaa4bcb5
sunwei <me@sunwei.xyz>
2023-09-29 09:18:41 +0800

    add test to obj

--- internal/domain/arch/valueobject/obj_test.go
+++ internal/domain/arch/valueobject/obj_test.go
@@ -190,3 +190,173 @@ func TestNewClass(t *testing.T) {
 		t.Errorf("Expected %d methods, but got %d", len(methodIdentifiers), len(class.Methods()))
 	}
 }
+
+func TestNewFunction_WithReceiver(t *testing.T) {
+	// 创建一个模拟的对象标识符和位置
+	mockIdentifier := &MockObject{
+		id: &ident{
+			name: "Object1",
+			pkg:  "pkg1",
+		},
+		name: "Object1",
+		dir:  "pkg1",
+		position: &MockPosition{
+			filename: "file1",
+			offset:   10,
+			line:     5,
+			column:   2,
+		},
+	}
+
+	// 调用 NewFunction 函数创建带有接收器的函数
+	function := NewFunction(mockIdentifier, mockIdentifier.id)
+
+	// 验证函数的属性是否正确设置
+	if function.obj.Identifier().Name() != mockIdentifier.Name() {
+		t.Errorf("Expected function.obj.Identifier().Name() to be %s, but got %s", mockIdentifier.Name(), function.obj.Identifier().Name())
+	}
+
+	if function.Receiver == nil {
+		t.Error("Expected function.Receiver to be a non-nil value")
+	}
+
+	if function.Receiver.name != mockIdentifier.id.Name() {
+		t.Errorf("Expected function.Receiver.name to be %s, but got %s", mockIdentifier.id.Name(), function.Receiver.name)
+	}
+
+	if function.Receiver.pkg != mockIdentifier.id.Dir() {
+		t.Errorf("Expected function.Receiver.pkg to be %s, but got %s", mockIdentifier.id.Dir(), function.Receiver.pkg)
+	}
+}
+
+func TestNewFunction_WithoutReceiver(t *testing.T) {
+	// 创建一个模拟的对象标识符和位置
+	mockIdentifier := &MockObject{
+		id: &ident{
+			name: "Object2",
+			pkg:  "pkg2",
+		},
+		name: "Object2",
+		dir:  "pkg2",
+		position: &MockPosition{
+			filename: "file2",
+			offset:   20,
+			line:     10,
+			column:   3,
+		},
+	}
+
+	// 调用 NewFunction 函数创建没有接收器的函数
+	function := NewFunction(mockIdentifier, nil)
+
+	// 验证函数的属性是否正确设置
+	if function.obj.Identifier().Name() != mockIdentifier.Name() {
+		t.Errorf("Expected function.obj.Identifier().Name() to be %s, but got %s", mockIdentifier.Name(), function.obj.Identifier().Name())
+	}
+
+	if function.Receiver != nil {
+		t.Error("Expected function.Receiver to be nil")
+	}
+}
+
+func TestNewAttr(t *testing.T) {
+	// 创建一个模拟的对象
+	mockObject := &MockObject{
+		id: &ident{
+			name: "Object1",
+			pkg:  "pkg1",
+		},
+		name: "Object1",
+		dir:  "pkg1",
+		position: &MockPosition{
+			filename: "file1",
+			offset:   10,
+			line:     5,
+			column:   2,
+		},
+	}
+
+	// 调用 NewAttr 函数创建属性对象
+	attr := NewAttr(mockObject)
+
+	// 验证属性对象的属性是否正确设置
+	if attr.obj.Identifier().Name() != mockObject.Name() {
+		t.Errorf("Expected attr.obj.Identifier().Name() to be %s, but got %s", mockObject.Name(), attr.obj.Identifier().Name())
+	}
+}
+
+func TestNewInterface(t *testing.T) {
+	// 创建一个模拟的对象
+	mockObject := &MockObject{
+		id: &ident{
+			name: "Object1",
+			pkg:  "pkg1",
+		},
+		name: "Object1",
+		dir:  "pkg1",
+		position: &MockPosition{
+			filename: "file1",
+			offset:   10,
+			line:     5,
+			column:   2,
+		},
+	}
+
+	// 创建一个包含模拟方法的对象切片
+	mockMethods := []arch.Object{
+		&MockObject{
+			id: &ident{
+				name: "Method1",
+				pkg:  "pkg1",
+			},
+			name: "Method1",
+			dir:  "pkg1",
+			position: &MockPosition{
+				filename: "file2",
+				offset:   20,
+				line:     10,
+				column:   3,
+			},
+		},
+		// 可以添加更多的方法对象
+	}
+
+	// 调用 NewInterface 函数创建接口对象
+	iface := NewInterface(mockObject, mockMethods)
+
+	// 验证接口对象的属性是否正确设置
+	if iface.obj.Identifier().Name() != mockObject.Name() {
+		t.Errorf("Expected iface.obj.Identifier().Name() to be %s, but got %s", mockObject.Name(), iface.obj.Identifier().Name())
+	}
+
+	// 验证接口对象的方法是否正确设置
+	if len(iface.methods) != len(mockMethods) {
+		t.Errorf("Expected %d methods, but got %d", len(mockMethods), len(iface.methods))
+	}
+}
+
+func TestNewGeneral(t *testing.T) {
+	// 创建一个模拟的对象
+	mockObject := &MockObject{
+		id: &ident{
+			name: "Object1",
+			pkg:  "pkg1",
+		},
+		name: "Object1",
+		dir:  "pkg1",
+		position: &MockPosition{
+			filename: "file1",
+			offset:   10,
+			line:     5,
+			column:   2,
+		},
+	}
+
+	// 调用 NewGeneral 函数创建 General 对象
+	general := NewGeneral(mockObject)
+
+	// 验证 General 对象的属性是否正确设置
+	if general.obj.Identifier().Name() != mockObject.Name() {
+		t.Errorf("Expected general.obj.Identifier().Name() to be %s, but got %s", mockObject.Name(), general.obj.Identifier().Name())
+	}
+}
