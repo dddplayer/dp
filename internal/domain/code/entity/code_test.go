3c0b6f87b5838a3fa96a261e21047d6c626f2f0e
sunwei <me@sunwei.xyz>
2023-10-04 08:31:33 +0800

    add test to code

--- internal/domain/code/entity/code_test.go
+++ internal/domain/code/entity/code_test.go
@@ -47,6 +47,44 @@ func TestVisit(t *testing.T) {
 	}
 }
 
+func TestVisitDeep(t *testing.T) {
+	// create a temporary directory to hold the test package
+	tempDir, err := ioutil.TempDir(".", "testpkg")
+	if err != nil {
+		t.Fatalf("failed to create temp dir: %v", err)
+	}
+	defer os.RemoveAll(tempDir)
+
+	// create a test package in the temporary directory
+	if err := createPkgTestPackage(tempDir); err != nil {
+		t.Fatalf("failed to create test package: %v", err)
+	}
+
+	// define the expected results
+	expectedNodeCount := 4
+	expectedLinkCount := 1
+
+	// define the callback functions
+	ch := &MockCodeHandler{}
+
+	// call the VisitFast function with the test package
+	c, err := NewCode(tempDir, "testpkg")
+	if err != nil {
+		t.Fatalf("NewCode failed with error: %v", err)
+	}
+	if err := c.VisitDeep(ch); err != nil {
+		t.Fatalf("VisitFast failed with error: %v", err)
+	}
+
+	if len(ch.nodes) != expectedNodeCount {
+		t.Errorf("Node callback did not process the expected number of nodes. Got: %d, Expected: %d", len(ch.nodes), expectedNodeCount)
+	}
+
+	if len(ch.links) != expectedLinkCount {
+		t.Errorf("Link callback did not process the expected number of links. Got: %d, Expected: %d", len(ch.links), expectedLinkCount)
+	}
+}
+
 // createTestPackage creates a test package in the specified directory
 func createPkgTestPackage(dir string) error {
 	// create some test files in the package directory
@@ -78,4 +116,26 @@ func (ch *MockCodeHandler) NodeHandler(node *code.Node) {
 
 func (ch *MockCodeHandler) LinkHandler(link *code.Link) {
 	ch.links = append(ch.links, link)
+}
+
+func TestNewGo_Error(t *testing.T) {
+	path := "github.com/example/mypackage"
+	domain := "example.com"
+
+	_, err := newGo(path, domain)
+
+	if err == nil {
+		t.Errorf("Expected an error, got no error")
+	}
+}
+
+func TestNewCode_Error(t *testing.T) {
+	mainPkgPath := "github.com/example/mypackage"
+	domain := "example.com"
+
+	_, err := NewCode(mainPkgPath, domain)
+
+	if err == nil {
+		t.Errorf("Expected an error, got no error")
+	}
 }
